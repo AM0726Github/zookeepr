7a6b7b6c70e4b371b1f8c7cbdf5ab9f0e95a52fd
AM0726Github <Margaryanalik7@gmail.com>
2022-02-20 14:01:42 -0800

    adding frontend files and implementing trough aplication by using express.ststic method for path

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -112,7 +113,9 @@ app.post('/api/animals', (req,res) => {
   }
 });
 
-
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
