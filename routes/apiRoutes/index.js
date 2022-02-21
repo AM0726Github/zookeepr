fe8e48b3735545774210fc44893e95953e4efcb6
AM0726Github <Margaryanalik7@gmail.com>
2022-02-21 06:14:52 -0800

    Done with zookepers front end and back end

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const animalRoutes = require('../apiRoutes/animalRoutes');
+const zookeprRoutes = require('../apiRoutes/zookeeperRoutes');
 
+router.use(zookeprRoutes);
 router.use(animalRoutes);
 
 module.exports = router;
